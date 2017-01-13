9c233b4a48e789e07699a20e7074f7d955e71e12
tindleaj <tindleaj@gmail.com>
2017-01-13 12:02:26 -0500

    initial commit

---
+++ includes/jetpack.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Jetpack Compatibility File
+ *
+ * @link https://jetpack.com/
+ *
+ * @package cah-starter
+ */
+
+/**
+ * Jetpack setup function.
+ *
+ * See: https://jetpack.com/support/infinite-scroll/
+ * See: https://jetpack.com/support/responsive-videos/
+ */
+function cah_starter_jetpack_setup() {
+	// Add theme support for Infinite Scroll.
+	add_theme_support( 'infinite-scroll', array(
+		'container' => 'main',
+		'render'    => 'cah_starter_infinite_scroll_render',
+		'footer'    => 'page',
+	) );
+
+	// Add theme support for Responsive Videos.
+	add_theme_support( 'jetpack-responsive-videos' );
+}
+add_action( 'after_setup_theme', 'cah_starter_jetpack_setup' );
+
+/**
+ * Custom render function for Infinite Scroll.
+ */
+function cah_starter_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		if ( is_search() ) :
+			get_template_part( 'partials/content', 'search' );
+		else :
+			get_template_part( 'partials/content', get_post_format() );
+		endif;
+	}
+}
