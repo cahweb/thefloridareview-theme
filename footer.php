ced301ad73d783970cc2880f0ee1d30f7f4b120f
tindleaj <tindleaj@gmail.com>
2017-01-13 14:38:23 -0500

    cleaning up underscores

--- footer.php
+++ footer.php
@@ -13,13 +13,11 @@
 
 	</div><!-- #content -->
 
+
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'cah-starter' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'cah-starter' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'cah-starter' ), 'cah-starter', '<a href="https://automattic.com/" rel="designer">Austin</a>' ); ?>
-		</div><!-- .site-info -->
+		
 	</footer><!-- #colophon -->
+
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -12,37 +12,45 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
 
-<?php wp_head(); ?>
+	<!-- UCF Assets -->
+	<link rel="icon" href="http://www.ucf.edu/img/pegasus-icon.png" type="image/png">
+	<script type="text/javascript" id="ucfhb-script" src="//universityheader.ucf.edu/bar/js/university-header.js"></script> 
+
+
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
+<div id="page" class="site container">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'cah-starter' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
+		<?php 
+			// Get image on frontpage post to use as the full width hero image
+			$image_id = get_post_thumbnail_id();
+			$image_url_array = wp_get_attachment_image_src($image_id, 'imagenail-size', true);
+			$image_url = $thumb_url_array[0];
+		?>
+
+		<!-- Branding -->
+		<div class="site-branding" style="background: url("<?php $image_url; ?>");">
+			
+			
+
+
 		</div><!-- .site-branding -->
 
+		<!-- Navigation -->
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'cah-starter' ); ?></button>
+
 			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
+
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
