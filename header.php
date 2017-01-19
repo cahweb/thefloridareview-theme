f8df3687559afd4be4a96755eef5ecf9046df66d
tindleaj <tindleaj@gmail.com>
2017-01-19 11:51:25 -0500

    added mobile nav and other nav tweaks

--- header.php
+++ header.php
@@ -42,7 +42,7 @@
 
 		<!-- Navigation -->
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'cah-starter' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'cah-starter' ); ?></button>
 
 			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
 
