3241ea358cc279e2483c78fde8f3a9b86bca1bcb
Ale Vecchi <ale@odessy.local>
2017-02-17 00:35:56 -0500

    added mobile support in navbar

--- header.php
+++ header.php
@@ -50,7 +50,7 @@
 
 			?></a>
 
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'cah-starter' ); ?></button>
+			<button class="menu-toggle" style="background-image: url(<?=get_stylesheet_directory_uri().'/public/images/menu.png'?>);" aria-controls="primary-menu" aria-expanded="false"></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?> 
 
 		</nav><!-- #site-navigation -->
