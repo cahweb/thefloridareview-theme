5e66b0af88bb1656b13f468168b82bc527668de6
Alessandro Vecchi <aleangel1212@gmail.com>
2017-03-03 13:19:50 +0000

    finished structure of issue page

--- page-issues.php
+++ page-issues.php
@@ -7,26 +7,32 @@
 
 			$query = new WP_Query(array(
 			    'post_type' => 'issue',
-			    'post_status' => 'publish',
-			    'posts_per_page' => 3
+			    'post_status' => 'publish'
 			));
 
+			$issues_per_row = 3;
 			$count = 0;
 			while ($query->have_posts()) {
 				$query->the_post();
-
-				if($count == 0)
+				$id = get_the_id();
+				$title = get_the_title();
+				$thumbnail = get_the_post_thumbnail_url($id);
+				$vol_num = get_post_meta($id,"vol-num",true);
+				$issue_num = get_post_meta($id,"issue-num",true);
+				$cov_date = get_post_meta($id,"cov-date",true);
+				$permalink = get_the_permalink();
+				if($count % $issues_per_row == 0)
 					echo "<div class=\"issue-display\">";
 		?>
-					<div class="issue-container">
-						<div class="issue-image"></div>
+					<div class="issue-container" onclick="location.href='<?=$permalink?>'">
+						<div class="issue-image" style="background-image: url(<?=$thumbnail?>);"></div>
 						<div class="issue-info">
-							<h4>41.5 | Summer 2017</h4>
+							<h4><?= $vol_num.".".$issue_num." | ".$cov_date ?></h4>
 						</div>
 					</div>
 
 		<?php
-				if($count == 0)
+				if($count % $issues_per_row == ($issues_per_row-1))
 					echo "</div>";
 				$count++;
 			}
