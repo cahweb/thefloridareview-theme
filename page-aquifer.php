72bfe8205f51eb07b18991e21d1b5d4a696d05fb
Mike Leavitt <mwleavitt@CNH-330030.local>
2017-03-15 17:10:45 -0400

    Messing with Category Display on Aquifer Page

--- page-aquifer.php
+++ page-aquifer.php
@@ -3,6 +3,9 @@
 <div id="primary" class="content-area border-top">
 	<main id="main" class="site-main" role="main">
 		<?php
+
+			$display_categories = array(6 => "Fiction", 8 => "Interview", 9 => "Non-Fiction", 10 => "Poetry", 15 => "Book Review", 16 => "Digital Stories", 17 => "Graphic Narrative");
+
 			the_title( '<h1 class="entry-title">', '</h1>' );
 
 			$query = new WP_Query(array(
@@ -16,11 +19,20 @@
 				$title = get_the_title();
 				$excerpt = get_the_excerpt();
 				$permalink = get_the_permalink();
+				$categories = get_the_category();
+
+				//Get the article's relevant categories for display in the article row
+				$categories_to_show = [];
+
+				foreach ($categories as $cat) {
+
+					if (in_array($cat->name, $display_categories))
+						array_push($categories_to_show, $cat->name);
+				}
 
 				// Skip the filler
-				if ($title == "Coming Soon!") {
+				if ($title == "Coming Soon!")
 					continue;
-				}
 
 				if(kdmfi_has_featured_image("author-image", $id) && !has_post_thumbnail())
 					$thumbnail = kdmfi_get_featured_image_src( "author-image", "small", $id );
@@ -35,9 +47,20 @@
 					<div class="article-thumb" style="background-image: url(<?=$thumbnail?>);"></div>
 					<div class="article-text">
 						<h4><?=$title?></h4>
-						<p><?=substr($excerpt,0,125)?></p>
+						<p><?=substr($excerpt,0,125)?></p></a>
+						<p class="category-list"><em><?php $cat_out = "";
+								 foreach ($categories_to_show as $cat_name) {
+
+									 $cat_out .= $cat_name;
+
+									 if (next($categories_to_show) !== false)
+									 	$cat_out .= ", ";
+								 }
+
+								 echo $cat_out;
+							?></em></p>
 					</div>
-				</a></div>
+				</div>
 
 		<?php
 			}
