db5a967e1c47c00e400e9da88aa00a2693c772f7
Mike Leavitt <michael.leavitt@ucf.edu>
2017-07-20 10:35:35 -0400

    tweaking index page

--- page-index.php
+++ page-index.php
@@ -1,8 +1,10 @@
 <?php get_header();?>
 
-<div id="primary" class="content-area border-top">
+<div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 		<?php
+
+			the_post();
 
 			the_title( '<h1 class="entry-title">', '</h1>' );
 
@@ -10,7 +12,7 @@
 
 		?>
 			<div id="cah-ajax-query-container"></div>
-			
+
 	</main>
 	<?php //get_sidebar();?>
 </div>
